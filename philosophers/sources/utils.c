fcf61cc5a4d5e4b7a7799e537654928fe63ab6cf
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-08-14 09:26:26 +0200

    Update utils.c

--- philosophers/sources/utils.c
+++ philosophers/sources/utils.c
@@ -38,31 +38,51 @@ int	ft_atoi(char *str)
 
 // Affiche les messages de statuts des philosophes et
 // des evenements de simulation
-void	message(t_arguments *table, int philo_number, char *message)
+void message(t_arguments *table, int philo_number, char *message)
 {
-	pthread_mutex_lock(&table->mutex_print);
-	printf("[%d]\tPhilosopher %d %s\n", get_time() - table->time_starter,
-		philo_number + 1, message);
-	if (message[0] != 'd')
-		pthread_mutex_unlock(&table->mutex_print);
+    // Verrouille le mutex 'mutex_print' pour éviter les conflits lors de l'affichage
+    pthread_mutex_lock(&table->mutex_print);
+
+    // Affiche le message formaté avec des informations sur le temps, le numéro du philosophe et le message
+    printf("[%d]\tPhilosopher %d %s\n", get_time() - table->time_starter, philo_number + 1, message);
+
+    // Vérifie si le message n'est pas un message de décès
+    if (message[0] != 'd')
+    {
+        // Déverrouille le mutex 'mutex_print'
+        pthread_mutex_unlock(&table->mutex_print);
+    }
 }
 
 // Met le thread en pause
-void	ft_usleep(__uint64_t time_in_milliseconde)
+void ft_usleep(__uint64_t time_in_millisecond)
 {
-	__uint64_t	time_start;
+    __uint64_t time_start;
 
-	time_start = 0;
-	time_start = get_time();
-	while ((get_time() - time_start) < time_in_milliseconde)
-		usleep(time_in_milliseconde / 10);
+    // Initialise la variable 'time_start' à 0
+    time_start = 0;
+
+    // Obtenir le temps de départ en utilisant la fonction 'get_time'
+    time_start = get_time();
+
+    // Boucle tant que le temps écoulé depuis le temps de départ est inférieur au temps spécifié
+    while ((get_time() - time_start) < time_in_millisecond)
+    {
+        // Fait une pause courte en utilisant la fonction 'usleep'
+        usleep(time_in_millisecond / 10);
+    }
 }
 
 // Renvoie le temps ecoule en ms depuis un moment de reference
-int	get_time(void)
+int get_time(void)
 {
-	static struct timeval	tv;
+    // Déclare une structure timeval 'tv' pour stocker le temps actuel
+    static struct timeval tv;
 
-	gettimeofday(&tv, NULL);
-	return (tv.tv_sec * (__uint64_t)1000 + (tv.tv_usec / 1000));
+    // Obtenir le temps actuel avec la fonction gettimeofday
+    gettimeofday(&tv, NULL);
+
+    // Calculer le temps en millisecondes et le retourner
+    return (tv.tv_sec * (__uint64_t)1000 + (tv.tv_usec / 1000));
 }
+
